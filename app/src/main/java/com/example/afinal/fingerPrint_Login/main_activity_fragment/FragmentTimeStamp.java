d30acf05e69de75ef3be63874bee76389e580fd9
arifdroid <arifdroiddeveloper@gmail.com>
2019-03-31 16:06:10 +0800

    >> work fingerprint >> logic check, pulling data works for admin >> working on getting data for user >> 24Mac 1PM, all work, now need to setup data structure, finalise, then code. >> 25Mac, data structure made up, now working on new log in structure with floating action button
    >>25Mac, 8:00PM, working login fragment.
    
    >>27Mac 12.40AM, getting checkbox works
    
    >>27Mac 11:14AM , checkbox, image, works.
    
    >>5;40PM, recycler firestore problem, androidx thing
    
    >>9.10PM , getting firebase cloud function working
    
    //31 Mac 2019
    
    >>2.45pm , all work, only sql and graph needs to be done
    
    >>4.00pm, working on graph. loading data

--- app/src/main/java/com/example/afinal/fingerPrint_Login/main_activity_fragment/FragmentTimeStamp.java
+++ app/src/main/java/com/example/afinal/fingerPrint_Login/main_activity_fragment/FragmentTimeStamp.java
@@ -10,13 +10,29 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.afinal.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class FragmentTimeStamp extends Fragment {
 
-    private TextView textView;
+    private LineChart chart;
 
     public FragmentTimeStamp() {
     }
+
+
+    //setup collection reference
+
+    private CollectionReference collectionReferenceTest;
+
 
     @Nullable
     @Override
@@ -26,7 +42,48 @@ public class FragmentTimeStamp extends Fragment {
         View rootView = inflater.inflate(R.layout.bottom_nav_timestamp_fragment, container, false);
         //textView = rootView.findViewById(R.id.bottom_nav_fragment_timeStamp_textView);
 
+        chart = rootView.findViewById(R.id.chartiD);
+
+        collectionReferenceTest = FirebaseFirestore.getInstance().collection("all_admin_doc_collections")
+                .document("ariff+60190_doc")
+                .collection("all_employee_thisAdmin_collection");
+
+        getTimeStampDataNow(collectionReferenceTest);
+
 
         return rootView;
+    }
+
+    private void getTimeStampDataNow(CollectionReference collectionReferenceTest) {
+
+        collectionReferenceTest.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                if(task.isSuccessful()){
+
+                    for(DocumentSnapshot documentSnapshot: task.getResult().getDocuments()){
+
+                        //snapshot of each employee data, pull out, every ts available for each document.
+                        //process may take more than 10 secs.
+                        //need to check from early check, what is today's day. then only pull relevant todays data.
+                        //for faster doc, cached it, load daily only. but for testing purpose,
+                        // load it all at once, update using observable.
+
+
+
+
+
+                    }
+
+
+
+                }else {
+
+                    //task not successful
+                }
+            }
+        });
+
     }
 }
