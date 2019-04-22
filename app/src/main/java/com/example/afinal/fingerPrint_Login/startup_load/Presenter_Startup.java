785bbd50731e8e35ff9784e7d3839e2c0cf67a10
arifdroid <arifdroiddeveloper@gmail.com>
2019-04-22 14:12:16 +0800

    >> 22 April 2.00 PM
    >> animation text working, and transition.

---
+++ app/src/main/java/com/example/afinal/fingerPrint_Login/startup_load/Presenter_Startup.java
@@ -0,0 +1,9 @@
+package com.example.afinal.fingerPrint_Login.startup_load;
+
+import java.util.Observable;
+
+public class Presenter_Startup extends Observable {
+
+
+
+}
--- app/src/main/java/com/example/afinal/fingerPrint_Login/startup_load/Startup_Activity.java
+++ app/src/main/java/com/example/afinal/fingerPrint_Login/startup_load/Startup_Activity.java
@@ -3,36 +3,52 @@ package com.example.afinal.fingerPrint_Login.startup_load;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.afinal.R;
 import com.example.afinal.fingerPrint_Login.fingerprint_login.FingerPrint_LogIn_Final_Activity;
+import com.hanks.htextview.base.AnimationListener;
+import com.hanks.htextview.base.HTextView;
 
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Startup_Activity extends AppCompatActivity {
+public class Startup_Activity extends AppCompatActivity implements Observer {
 
     private Timer timer;
     private int countHere;
+    private Presenter_Startup presenter;
+
+    private HTextView hTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup_);
 
-        ConstraintLayout constraintLayout = findViewById(R.id.layoutStartupID);
-        AnimationDrawable animationDrawable
+        hTextView = findViewById(R.id.scaletextID);
 
-                = (AnimationDrawable) constraintLayout.getBackground();
+
+        ConstraintLayout constraintLayout = findViewById(R.id.layoutStartupID);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
 
         countHere=0;
+
+        presenter = new Presenter_Startup();
 
         animationDrawable.setEnterFadeDuration(1200);
         animationDrawable.setExitFadeDuration(2500);
         animationDrawable.start();
+
+
+
+
 
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
@@ -40,16 +56,44 @@ public class Startup_Activity extends AppCompatActivity {
             public void run() {
                 countHere++;
 
-                if(countHere==8){
+                if(countHere==2){
+
+                           hTextView.animateText("\"Hello..\"");
+
+                }
+                if(countHere==4){
+
+                           // hTextView.setTextSize(30);
+                          hTextView.animateText("\"Welcome..\"");
+
+                }
+                if(countHere==6){
+
+                         hTextView.animateText("\"Loggin in Now..\"");
+
+                }
+
+                if(countHere==9){
                     timer.cancel();
+
+                    ActivityOptions activityOptions = ActivityOptions.makeCustomAnimation(Startup_Activity.this,R.anim.fadein,R.anim.fadeout);
+
                     Intent intent = new Intent(Startup_Activity.this, FingerPrint_LogIn_Final_Activity.class);
-                    startActivity(intent);
+                    startActivity(intent,activityOptions.toBundle());
                     finish();
+
+
                 }
 
 
             }
         },0,1200);
+
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
 
     }
 }
